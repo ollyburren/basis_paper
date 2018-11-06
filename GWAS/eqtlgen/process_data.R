aec9e82280c6c90df231d6696110d94c2a6a6a5b
ollyburren <oliver.burren@cimr.cam.ac.uk>
2018-11-06 18:59:35 +0000

    added eqtlgen processing

---
+++ GWAS/eqtlgen/process_data.R
@@ -0,0 +1,79 @@
+## process both cis and trans signals from Võsa,U. et al. (2018) Unraveling the polygenic architecture of complex traits using blood eQTL meta-analysis. bioRxiv, 447367.
+
+## note that these are large files - I used UNIX to create a list of unique variants in
+## order to facilitate filtering.
+
+EQTL_GEN_MAN <- '/home/ob219/share/Data/expr/eqtlgen/cis_manifest.tab'
+SNP_MANIFEST <- '/home/ob219/share/as_basis/GWAS/snp_manifest/gwas_june.tab'
+e.DT <- fread(EQTL_GEN_MAN)
+setnames(e.DT,c('id','chr','position','effect','other'))
+e.DT <- e.DT[id!='SNP',]
+e.DT[,pid:=paste(chr,position,sep=':')]
+snp.DT <- fread(SNP_MANIFEST)
+keep.id <- e.DT[pid %in% snp.DT$pid,]$id
+ID_FILE <- "/home/ob219/share/as_basis/GWAS/eqtlgen/gwas_june_match.tab"
+write(keep.id,file=ID_FILE)
+CIS_FILE <- '/home/ob219/share/Data/expr/eqtlgen/cis-eQTLs_full_20180905.txt.gz'
+OUT_FILE <- '/home/ob219/share/as_basis/GWAS/eqtlgen/cis-eQTLs_full_20180905.filtered.txt'
+cmd <- sprintf("zcat %s | grep -f %s > %s",CIS_FILE,ID_FILE,OUT_FILE)
+system(cmd)
+#zcat /home/ob219/share/Data/expr/eqtlgen/cis-eQTLs_full_20180905.txt.gz | grep -f /home/ob219/share/as_basis/GWAS/eqtlgen/gwas_june_match.tab > /home/ob219/share/as_basis/GWAS/eqtlgen/cis-eQTLs_full_20180905.filtered.txt
+## above does not work as get out of mem error instead us data.table
+DT<-fread("zcat /home/ob219/share/Data/expr/eqtlgen/cis-eQTLs_full_20180905.txt.gz")
+f.DT<-DT[SNP %in% keep.id,]
+
+saveRDS(f.DT,"/home/ob219/share/as_basis/GWAS/eqtlgen/cis-eQTLs_full_20180905.filtered.RDS")
+f.DT<-readRDS("/home/ob219/share/as_basis/GWAS/eqtlgen/cis-eQTLs_full_20180905.filtered.RDS")
+## next do the same for trans eqtl
+EQTL_GEN_MAN_TRANS <- '/home/ob219/share/Data/expr/eqtlgen/trans_manifest.tab'
+DT.t<-fread("/home/ob219/share/Data/expr/eqtlgen/trans-eQTLs_full_20180905.txt")
+e.DT <- fread(EQTL_GEN_MAN_TRANS)
+setnames(e.DT,c('id','chr','position','effect','other'))
+e.DT <- e.DT[id!='SNP',]
+e.DT[,pid:=paste(chr,position,sep=':')]
+snp.DT <- fread(SNP_MANIFEST)
+keep.id <- e.DT[pid %in% snp.DT$pid,]$id
+f.DT.t<-DT.t[SNP %in% keep.id,]
+
+## merge into one file and convert Z scores to the beta scale by multiplying through by se of beta due to variance
+
+f.DT.t[,type:='trans']
+f.DT[,type:='cis']
+
+basis.DT <- rbind(f.DT.t,f.DT)
+
+basis.DT[,pid:=paste(SNPChr,SNPPos,sep=':')]
+basis.DT <- basis.DT[,.(pid,Zscore,risk.allele=AssessedAllele,other.allele=OtherAllele,ensg=Gene,NrSamples)]
+##merge with the manifest to get MAF so can convert to the correct scale
+M <- merge(basis.DT,snp.DT,by='pid')
+
+M[,maf:=ifelse(ref_a1.af>0.5,1-ref_a1.af,ref_a1.af)]
+
+# this to get a derived beta that takes into account differing MAF's and sample sizes.
+M[,der.beta:=1/(sqrt(2 * NrSamples) * sqrt(maf * (1-maf))) * Zscore]
+
+library(annotSnpStats)
+alleles <- data.table(pid=M$pid,al.x = paste(M$ref_a1,M$ref_a2,sep='/'),al.y=paste(M$risk.allele,M$other.allele,sep='/'))
+alleles <- alleles[!duplicated(pid),]
+#alleles <- M[,list(al.x=paste(uk10_A1,uk10_A2,sep='/'),al.y=paste(a1,a2,sep='/')),by='pid']
+## to make quick
+align.class <- rep('match',nrow(alleles))
+idx<-which(alleles$al.x!=alleles$al.y)
+x.alleles <- alleles[idx,]$al.x
+names(x.alleles)<-alleles[idx,]$pid
+y.alleles <-  alleles[idx,]$al.y
+names(y.alleles)<-names(x.alleles)
+align.class[idx] <- g.class(x.alleles,y.alleles)
+print(table(align.class))
+alleles[,g.class:=align.class]
+M <- merge(M,alleles[,.(pid,g.class)],by='pid',all.x=TRUE)
+M[g.class=='rev',der.beta:=der.beta*-1]
+M.out <- M[,.(pid,a1=ref_a1,a2=ref_a2,or=der.beta,p.value=2*pnorm(abs(Zscore),lower.tail=FALSE),ensg)]
+by.gene<-split(M.out,M.out$ensg)
+OUT_DIR <- '/home/ob219/rds/rds-cew54-wallace-share/as_basis/GWAS/sum_stats/eqtlgen/'
+for(n in names(by.gene)){
+  message(n)
+  out <- by.gene[[n]][,.(pid,a1,a2,or,p.value)]
+  fname <- file.path(OUT_DIR,sprintf("%s.tab",n))
+  write.table(out,file=fname,quote=FALSE,row.names=FALSE,sep="\t")
+}
---
+++ GWAS/eqtlgen/process_eqtlgen.R
@@ -0,0 +1,106 @@
+## work out the list of variants reqiured
+
+library(optparse)
+
+TEST<-FALSE
+option_list = list(
+        make_option(c("-f", "--fname"), type="character", default=NULL,
+              help="index of phenotype to process ", metavar="character")
+            )
+if(!TEST){
+  opt_parser = OptionParser(option_list=option_list);
+  args = parse_args(opt_parser)
+  if (is.null(args$integer)){
+	   print_help(opt_parser)
+	    stop("Supply an file containing a list of genes to process", call.=FALSE)
+    }
+}else{
+  args <- list(integer=89)
+}
+
+gene.list <- read(args$fname,"character")
+
+#gene.list <- c('ENSG00000270170','ENSG00000270172','ENSG00000270175','ENSG00000270177','ENSG00000270179','ENSG00000270184')
+
+## to use maf estimate of se remove ss prefix !
+
+SHRINKAGE_METHOD<-'ws_emp_shrinkage'
+## just the one shrinkage file
+SHRINKAGE_FILE <- '/home/ob219/share/as_basis/GWAS/support/ss_shrinkage_gwas.RDS'
+BASIS_FILE <- '/home/ob219/share/as_basis/GWAS/support/ss_basis_gwas.RDS'
+DATA_DIR <- '/home/ob219/rds/rds-cew54-wallace-share/as_basis/GWAS/sum_stats/eqtlgen'
+SNP_MANIFEST_FILE <-'/home/ob219/share/as_basis/GWAS/snp_manifest/gwas_june.tab'
+OUT_DIR <- '/home/ob219/rds/rds-cew54-wallace-share/as_basis/GWAS/eqtlgen_projections'
+
+
+## running on the queue
+if(FALSE){
+  files <- list.files(path=DATA_DIR,pattern="*.tab",full.names=FALSE)
+  ensgs <- basename(files) %>% gsub("\\.tab","",.)
+  CHUNK_SIZE <- 100
+  gchunk <- split(ensgs, ceiling(seq_along(ensgs)/CHUNK_SIZE))
+  for(i in seq_along(gchunk)){
+    fname <- file.path('/home/ob219/tmp/qstuff/eqtlgen/genelists',sprintf("gene_%d.txt",i))
+    write(gchunk[[i]],file=fname)
+  }
+  files <- list.files(path='/home/ob219/tmp/qstuff/eqtlgen/genelists',pattern="*.txt",full.names=TRUE)
+  cmds <- sapply(files,function(f){
+  #cmds <- sapply(c(165,159,78,167,116,57,166),function(i){
+    sprintf("Rscript /home/ob219/git/basis_paper/GWAS/process_bb_self_reported_disease_august_2018.R -i %d",i)
+  })
+  write(cmds,file="~/tmp/qstuff/gwas_bb_disease_proj_2018.txt")
+}
+
+shrink.DT <- readRDS(SHRINKAGE_FILE)
+shrink.DT<-shrink.DT[,c('pid',shrink=SHRINKAGE_METHOD),with=FALSE]
+setkey(shrink.DT,'pid')
+pc.emp <- readRDS(BASIS_FILE)
+snp.DT <- fread(SNP_MANIFEST_FILE)
+dummy.DT <- snp.DT[,.(pid,uid='DUMMY:-999',shrunk.beta=0)]
+
+all.DT <- lapply(gene.list,function(ensg){
+  lf <- file.path(DATA_DIR,sprintf("%s.tab",ensg))
+  DT <- fread(lf)[,uid:=ensg]
+  merge(DT,shrink.DT,by.x='pid',by.y='pid')[,shrunk.beta:=or * get(`SHRINKAGE_METHOD`)][,.(pid,uid,shrunk.beta)]
+}) %>% rbindlist
+
+
+
+all.DT <- rbind(all.DT,dummy.DT)
+all.DT <- melt(all.DT,id.vars=c('pid','uid'),measure.vars='shrunk.beta')
+setkey(all.DT,'pid')
+r.DT <- dcast(all.DT,pid~uid+variable,fill=0)
+mat <- as.matrix(r.DT[,-1])
+rownames(mat) <- r.DT[[1]]
+bc <- predict(pc.emp,newdata=t(mat))
+res.DT <- data.table(trait = rownames(bc)  %>% gsub("_shrunk.beta","",.),bc)[trait!='DUMMY:-999',]
+
+
+
+
+saveRDS(res.DT,file=sprintf("%s%s.RDS",OUT_DIR,fname))
+message(sprintf("Wrote to %s%s.RDS",OUT_DIR,fname))
+#file.remove(file.path(odir,med$ofile[i]))
+
+
+
+if(FALSE){
+  library(cowplot)
+  OUT_DIR <- '/home/ob219/rds/hpc-work/as_basis/bb/summary_stats_20180731/self_reported_disease/june_10k/'
+  BASIS_FILE <- '/home/ob219/rds/hpc-work/as_basis/support/basis_june10k.RDS'
+  fs <- list.files(path=OUT_DIR,pattern="*.RDS",full.names=TRUE)
+  res.DT <- lapply(fs,readRDS) %>% rbindlist
+  pc.emp <- readRDS(BASIS_FILE)
+  basis.DT <- data.table(trait=rownames(pc.emp$x),pc.emp$x,cat='basis')
+  res.DT[,cat:='bb']
+  plot.DT <- rbind(res.DT,basis.DT)
+  plot.DT[,label:=gsub("\\.[0-9]*mg.*","",trait)]
+  ggplot(plot.DT,aes(x=PC1,y=PC4,label=label,col=cat)) + geom_point() + geom_text()
+  ## compute Z scores for chris
+  mdt<-melt(res.DT,id.vars='trait',measure.vars=sprintf("PC%d",1:11))
+  mdt[,c('mean','sd'):=list(mean(value),sd(value)),by='variable']
+  mdt[,Z:=(value-mean)/sd]
+  mdt[,p.value:=pnorm(abs(Z),lower.tail=FALSE) * 2]
+  mdt[,p.adj:=p.adjust(p.value,method="fdr"),by=variable]
+  save(mdt,file="/home/ob219/rds/rds-cew54-wallace-share/as_basis/bb/basis_june10k_mself_reported_disease.RDS")
+}
